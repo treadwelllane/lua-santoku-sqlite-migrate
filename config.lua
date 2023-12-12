bbef8688aa57a794ad89d2979093d1662923603c
broma0 <121064585+broma0@users.noreply.github.com>
2023-12-12 22:00:34 +0000

    migrate.lua: add M.migrate
    lib/santoku/sqlite/migrate.lua
      + M.migrate
    
    also: .gitignore, Makefile, config.lua, docs/todo.md

---
+++ config.lua
@@ -0,0 +1,31 @@
+local env = {
+
+  name = "santoku-sqlite-migrate",
+  version = "0.0.3-1",
+  variable_prefix = "TK_SQLITE_MIGRATE",
+  license = "MIT",
+  public = true,
+
+  dependencies = {
+    "lua >= 5.1",
+    "santoku-fs >= 0.0.2-1",
+    "santoku-sqlite >= 0.0.2-1",
+  },
+
+  test_dependencies = {
+    "santoku-test >= 0.0.2-1",
+    "luassert >= 1.9.0-1",
+    "luacheck >= 1.1.0-1",
+    "luacov >= 0.15.0-1",
+  },
+
+}
+
+env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
+env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
+env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
+
+return {
+  env = env,
+}
+
---
+++ lib/santoku/sqlite/migrate.lua
@@ -0,0 +1,63 @@
+local err = require("santoku.err")
+local gen = require("santoku.gen")
+local op = require("santoku.op")
+local tup = require("santoku.tup")
+local fs = require("santoku.fs")
+local str = require("santoku.string")
+
+local M = {}
+
+M.migrate = function (db, opts)
+  return err.pwrap(function (check)
+
+    local files
+
+    if type(opts) == "string" then
+
+      local fp = opts
+      files = fs.files(fp):map(check):vec():sort(str.compare):map(function (fp)
+        return tup(fs.basename(fp), function ()
+          return check(fs.readfile(fp))
+        end)
+      end)
+
+    elseif type(opts) == "table" then
+
+      local tbl = opts
+      files = gen.keys(tbl):vec():sort(str.compare):map(function (fp)
+        return tup(fs.basename(fp), function ()
+          return opts[fp]
+        end)
+      end)
+
+    else
+      return false, "invalid argument type to migrate: " .. type(opts)
+    end
+
+    check(db:begin())
+
+    check(db:exec([[
+      create table if not exists migrations (
+        id integer primary key,
+        filename text not null
+      );
+    ]]))
+
+    local get_migration = check(db:getter("select id from migrations where filename = ?", "id"))
+    local add_migration = check(db:inserter("insert into migrations (filename) values (?)"))
+
+    gen.ivals(files):map(op.call):filter(function (fp)
+      return not check(get_migration(fp))
+    end):map(function (fp, read)
+      return fp, read()
+    end):each(function (fp, data)
+      check(db:exec(data))
+      check(add_migration(fp))
+    end)
+
+    check(db:commit())
+
+  end)
+end
+
+return M
