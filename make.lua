2b10961a54decdfa397c1c95797724c736c65f75
broma0 <121064585+broma0@users.noreply.github.com>
2024-01-01 18:21:27 +0000

    update .gitignore Makefile config.lua make.lua todo.md

--- make.lua
+++ make.lua
@@ -8,15 +8,16 @@ local env = {
 
   dependencies = {
     "lua >= 5.1",
-    "santoku-fs >= 0.0.2-1",
-    "santoku-sqlite >= 0.0.4-1",
+    "santoku-fs >= 0.0.13-1",
+    "santoku-sqlite >= 0.0.6-1",
   },
 
-  test_dependencies = {
-    "santoku-test >= 0.0.2-1",
-    "luassert >= 1.9.0-1",
-    "luacheck >= 1.1.0-1",
-    "luacov >= 0.15.0-1",
+  test = {
+    dependencies = {
+      "santoku-test >= 0.0.7-1",
+      "luassert >= 1.9.0-1",
+      "luacov >= 0.15.0-1",
+    }
   },
 
 }
@@ -26,6 +27,7 @@ env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
 return {
+  type = "lib",
   env = env,
 }
 
