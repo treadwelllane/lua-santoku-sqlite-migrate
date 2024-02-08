4ce5d9cda9c4ce5a7f43ddd4b70afc37bd801823
broma0 <121064585+broma0@users.noreply.github.com>
2024-02-08 00:48:51 +0000

    bump version to 0.0.10-1

--- make.lua
+++ make.lua
@@ -1,13 +1,14 @@
 local env = {
 
   name = "santoku-sqlite-migrate",
-  version = "0.0.9-1",
+  version = "0.0.10-1",
   variable_prefix = "TK_SQLITE_MIGRATE",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
+    "santoku >= 0.0.184-1",
     "santoku-fs >= 0.0.25-1",
     "santoku-sqlite >= 0.0.9-1",
   },
