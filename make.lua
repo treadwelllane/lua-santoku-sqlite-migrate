ced70c18549f22ceb8432ce89c7f45960a9f9a56
broma0 <121064585+broma0@users.noreply.github.com>
2025-11-28 20:17:39 -0500

    update make.lua migrate.lua

--- make.lua
+++ make.lua
@@ -1,23 +1,13 @@
 local env = {
-
   name = "santoku-sqlite-migrate",
-  version = "0.0.15-1",
+  version = "0.0.16-1",
   variable_prefix = "TK_SQLITE_MIGRATE",
   license = "MIT",
   public = true,
-
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.294-1",
-    "santoku-fs >= 0.0.34-1",
+    "santoku >= 0.0.297-1",
   },
-
-  test = {
-    dependencies = {
-      "luacov >= 0.15.0-1",
-    }
-  },
-
 }
 
 env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
@@ -25,7 +15,7 @@ env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
 return {
-  
+
   env = env,
 }
 
