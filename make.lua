e20616f621be127ef77728ac878862da835bef35
broma0 <121064585+broma0@users.noreply.github.com>
2026-04-16 22:56:55 -0400

    bump version to 0.0.20-1

--- make.lua
+++ make.lua
@@ -1,16 +1,16 @@
 local env = {
   name = "santoku-sqlite-migrate",
-  version = "0.0.19-1",
+  version = "0.0.20-1",
   variable_prefix = "TK_SQLITE_MIGRATE",
   license = "MIT",
   public = true,
   dependencies = {
-    "lua >= 5.1",
+    "lua == 5.1",
     "santoku >= 0.0.310-1",
   },
 }
 
-env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
+env.homepage = "https://github.com/birchpointswe/lua-" .. env.name
 env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
